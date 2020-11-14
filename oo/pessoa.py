4afde94d4008c4cc965c133203f6f1eafdea8901
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2020-11-14 16:19:00 -0300

    criado atributo de classe Olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome = None, idade =35): #dander init
         self.idade = idade
         self.nome = nome
@@ -19,5 +20,11 @@ if __name__ == "__main__":
         print(filho.nome)
     luciano.sobrenome = 'Ramalho' #cria um atributo
     del luciano.filhos #deleta um atributo
-    print(luciano.__dict__)
+    luciano.olhos = 1
+    del luciano.olhos
+    print(luciano.__dict__) #dict mostra os atributos de instancias de um ojeto
     print(renzo.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(renzo.olhos)
+    print( id(Pessoa.olhos), id(renzo.olhos), id(luciano.olhos))
